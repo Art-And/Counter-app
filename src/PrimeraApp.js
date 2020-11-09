b85683f7c2a53a67d9367be7cd5de1b3590e0739
Andres Islas <hir.islas@outlook.com>
2020-11-09 13:22:04 -0600

    Se agraga const para JSX

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const PrimeraApp = () => (
-  <>
-    <h1>Test</h1>
-    <p>Este es un Test</p>
-  </>
-);
+const PrimeraApp = () => {
+
+  const testWord = 'Test';
+
+  return (
+    <>
+      <h1>{ testWord }</h1>
+      <p>Este es un Test</p>
+    </>
+  );
+};
 
 export default PrimeraApp;
 
