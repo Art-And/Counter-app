dfd993663a9ebe74228fcd93b1e5273a58b37efa
Andres Islas <hir.islas@outlook.com>
2020-11-09 20:30:24 -0600

    Se agrega hook del counter

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
 
+  const [ counter, setCounter ] = useState(0);
+
   const handleClick = () =>{
-    console.log('mas uno +1');
+    setCounter(counter + 1);
   }
 
     return(
     <>
       <h1>CounterApp</h1>
-      <h2>{ value }</h2>
+      <h2>{ counter }</h2>
 
       <button onClick={handleClick} >+1</button>
     </>
